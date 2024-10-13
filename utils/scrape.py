41ef4c3161088840dc77e988a63014e1404561ea
ldjb <git@ldjb.uk>
2024-10-13 03:30:01 +0100

    Better error handling

--- utils/scrape.py
+++ utils/scrape.py
@@ -1,6 +1,7 @@
 import json
 import re
 import requests
+import time
 from email.message import Message
 
 session = requests.Session()
@@ -8,11 +9,22 @@ response = session.get('https://beta-naptan.dft.gov.uk/download/la')
 match = re.search('const localAuthoritiesList = (.*);', response.text)
 local_authorities = json.loads(match.group(1))
 for local_authority in local_authorities:
+    local_authority_name = local_authority['localAuthorityName']
     atco_code_prefix = local_authority['atcoCodePrefix']
+    print(f'Downloading NaPTAN files for {local_authority_name}')
     for format in ('xml', 'csv'):
-        response = session.get(f'https://naptan.api.dft.gov.uk/v1/access-nodes?atcoAreaCodes={atco_code_prefix}&dataFormat={format}')
-        message = Message()
-        message['content-disposition'] = response.headers['content-disposition']
-        filename = message.get_filename()
-        with open(filename, 'wb') as file:
-            file.write(response.content)
+        for attempt in range(3):
+            if attempt >= 1:
+                print('Download failed; trying again')
+            try:
+                response = session.get(f'https://naptan.api.dft.gov.uk/v1/access-nodes?atcoAreaCodes={atco_code_prefix}&dataFormat={format}')
+                message = Message()
+                message['content-disposition'] = response.headers['content-disposition']
+                filename = message.get_filename()
+                with open(filename, 'wb') as file:
+                    file.write(response.content)
+                time.sleep(1)
+            except:
+                pass
+            else:
+                break
