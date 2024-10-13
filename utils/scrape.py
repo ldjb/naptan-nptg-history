99dc29246c3868ed2673afa37eb584d201335806
ldjb <git@ldjb.uk>
2024-10-13 04:06:55 +0100

    Add generation of local_authorities.csv

--- utils/scrape.py
+++ utils/scrape.py
@@ -1,3 +1,4 @@
+import csv
 import json
 import re
 import requests
@@ -25,12 +26,19 @@ session = requests.Session()
 response = session.get('https://beta-naptan.dft.gov.uk/download/la')
 match = re.search('const localAuthoritiesList = (.*);', response.text)
 local_authorities = json.loads(match.group(1))
-for local_authority in local_authorities:
-    local_authority_name = local_authority['localAuthorityName']
-    atco_code_prefix = local_authority['atcoCodePrefix']
-    print(f'Downloading NaPTAN files for {local_authority_name}')
-    for format in ('xml', 'csv'):
-        download(f'https://naptan.api.dft.gov.uk/v1/access-nodes?atcoAreaCodes={atco_code_prefix}&dataFormat={format}', session)
+with open('local_authorities.csv', 'w', newline='') as csvfile:
+    fieldnames = ['atcoCodePrefix', 'localAuthorityName']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for local_authority in local_authorities:
+        local_authority_name = local_authority['localAuthorityName']
+        atco_code_prefix = local_authority['atcoCodePrefix']
+        match = re.search('(.*) \(\d{3}\)', local_authority['localAuthorityName'])
+        local_authority['localAuthorityName'] = match.group(1)
+        writer.writerow(local_authority)
+        print(f'Downloading NaPTAN files for {local_authority_name}')
+        for format in ('xml', 'csv'):
+            download(f'https://naptan.api.dft.gov.uk/v1/access-nodes?atcoAreaCodes={atco_code_prefix}&dataFormat={format}', session)
 print('Downloading NPTG files')
 download('https://naptan.api.dft.gov.uk/v1/nptg', session)
 download('https://naptan.api.dft.gov.uk/v1/nptg/localities', session)
